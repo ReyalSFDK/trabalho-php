4ede8fc42c06adf45ad0e7c568536c5f2c45bf09
Emanuel Souza <emanuel-souza1@hotmail.com>
2019-10-02 11:29:38 -0300

    commit carro

--- classes/carro.php
+++ classes/carro.php
@@ -15,8 +15,9 @@ if ($id_pacote === PACOTE_INVALIDO) {
     header("Location: pacote.php");
 }
 
-// Faz uma query para buscar no banco o carro
-$query = $con->query("SELECT c.id,c.nome FROM carro AS c INNER JOIN pacote AS p ON c.id_tipo = p.id_tipo");
+// Faz uma query para buscar no banco o carro de acordo com o pacote escolhido
+$query = $con->query("SELECT c.id,c.nome FROM carro AS c INNER JOIN pacote AS p ON c.id_tipo = p.id_tipo WHERE p.id = ?");
+$query->bindParam(1, $id_pacote);
 $query->execute();
 
 if ($query->rowCount() < 1) {
