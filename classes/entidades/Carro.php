262c07a99248342e07dffa327b6a1873a0ea7937
ReyalSFDK <luan.oliveira.7587@gmail.com>
2019-10-19 11:51:22 -0300

    Add files via upload

---
+++ classes/entidades/Carro.php
@@ -0,0 +1,47 @@
+<?
+class Carro{
+
+    /**
+     * @var string | null
+     */
+
+    private $id = null;
+
+    /**
+     * @var string | null
+     */
+
+    private $nome;
+
+    /**
+     * @var string | null
+     */
+
+    private $id_tipo;
+
+    public function getId(): ?string {
+
+        return $this->id;
+    }
+    
+    public function getNome(): string {
+
+        return $this->nome;
+    }
+    
+    public function getIdTipo(): string {
+
+        return $this->id_tipo;
+    }
+
+    static function  getDatabaseCarro(object $databaseData): self {
+        $carro = new Carro();
+        $carro->setNome($databaseData->nome);
+
+        if ($databaseData->id) {
+            $this->id = $databaseData->id;
+        }
+
+        return $carro;
+    }
+}
---
+++ classes/entidades/Cliente.php
@@ -0,0 +1,77 @@
+<?
+class Cliente {
+    /**
+     * @var string | null
+     */
+    private $id = null;
+
+    /**
+     * @var string | null
+     */
+    private $nome;
+
+        /**
+     * @var string | null
+     */
+    private $email = null;
+
+    /**
+     * @var string | null
+     */
+    private $cpf;
+
+    /**
+     * @var string | null
+     */
+    private $telefone;
+
+    public function getId(): ?string {
+        return $this->id;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self {
+        $this->nome = $email;
+        return $this;
+    }
+
+    public function getCpf(): string {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): self {
+        $this->nome = $cpf;
+        return $this;
+    }
+
+    public function getTelefone(): string {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): self {
+        $this->nome = $telefone;
+        return $this;
+    }
+    static function  getDatabaseCliente(object $databaseData): self {
+        $cliente = new Cliente();
+        $cliente->setNome($databaseData->nome);
+
+        if ($databaseData->id) {
+            $this->id = $databaseData->id;
+        }
+
+        return $cliente;
+    }
+}
