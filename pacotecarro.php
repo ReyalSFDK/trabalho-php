c0204aad5813de5cff0c5e67893d71bc46185f6f
MaiaTha <thais.maindrade@gmail.com>
2019-12-03 20:19:50 -0300

    removed file and added screen to rent car

---
+++ pacotecarro.php
@@ -0,0 +1,54 @@
+<?php
+// Bootstrap
+require_once("./classes/Bootstrap.php");
+// Core
+$core = $bootstrap->getCore();
+// Setar o cabeçalho
+echo $core->setHeader("Inicio");
+?>
+<nav class="navbar navbar-dark bg-dark" style="display: flex; justify-content: center;">
+    <img src="https://imagensemoldes.com.br/wp-content/uploads/2018/01/Logo-Filme-Carros-01.png" alt="" width=50px
+         heigth=50px>
+</nav>
+
+<div class="container" style="margin-top:35px;">
+        <div class="form-group">
+            <label>Plano</label>
+            <span class="input-group-text" id="inputGroup-sizing-default">Premium</span>
+        </div>
+        <div class="form-group">
+            <label>Tipo</label>
+            <span class="input-group-text" id="inputGroup-sizing-default">SUV</span>
+        </div>
+        <div class="form-group">
+            <label>Preço do pacote</label>
+            <span class="input-group-text" id="inputGroup-sizing-default">100 reais</span>
+        </div>
+
+        <form>
+            <div class="form-group">
+                <label>Nome</label>
+                <input type="name" class="form-control" placeholder="Ex Pen Blue">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" placeholder="dragonballgt@gmail.com">
+            </div>
+            <div class="form-group">
+                <label>Cartão</label>
+                <input type="text" class="form-control" placeholder="4000000000000000000000000">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>CPF</label>
+                    <input type="text" class="form-control" placeholder="02558558525">
+                </div>
+                <div class="form-group col-md-6">
+                    <label>Telefone</label>
+                    <input type="text" class="form-control"  placeholder="71982509571">
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+</div>
